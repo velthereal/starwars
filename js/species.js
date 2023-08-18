ef8492cd75e0b6550e318af90a4ed2760a385870
velthereal <lera2005olex@gmail.com>
2023-08-18 16:54:09 +0300

    finished project

--- js/species.js
+++ js/species.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', ()=>{
 	getPersons(1, true);
 });
 
@@ -27,9 +27,10 @@ function showAllPerson(data){
 	content.innerHTML = '';
 	data.forEach(element => {
 		let str = `<div class="card mb-3">
-			<h3 class="card-header">${element.name}</h3>
-			<img src="https://starwars-visualguide.com/assets/img/species/${element.url.match(/\/([0-9]*)\/$/)[1]}.jpg" class="d-block user-select-none"></div>`;
-			content.insertAdjacentHTML('beforeend', str);
+							<h3 class="card-header">${element.name}</h3>
+							<img src="https://starwars-visualguide.com/assets/img/species/${element.url.match(/\/([0-9]*)\/$/)[1]}.jpg" class="d-block user-select-none">
+						</div>`;
+		content.insertAdjacentHTML('beforeend', str);
 	});
 	showPerson(data);
 }
@@ -52,23 +53,28 @@ function createPagination(all, current){
 	let number = parseInt(all / current) + (all / current > parseInt(all / current) ? 1 : 0);
 	for (let i = 0; i < number; i++) {
 		if(i === 0){
-			line += `  <li class="page-item active"><a class="page-link" href="#">${i+1}</a></li>`;
+			line += `<li class="page-item active"><a class="page-link" href="#">${i+1}</a></li>`;
 			continue;
 		}
-		line += `  <li class="page-item"><a class="page-link" href="#">${i+1}</a></li>`;
+		line += `<li class="page-item"><a class="page-link" href="#">${i+1}</a></li>`;
 	}
 	document.querySelector('.pagination li:first-child').insertAdjacentHTML('afterend', line);
 }
 
 function showPerson(data){
 	let blocks = document.querySelectorAll('.content div.card');
+	for(let i = 0; i < blocks.length; i++){
+		blocks[i].children[1].onerror = function(){
+			blocks[i].children[1].src = 'https://starwars-visualguide.com/assets/img/placeholder.jpg';
+		}
+	}
 	for (let i = 0; i < blocks.length; i++) {
-		blocks[i].addEventListener('click', () => {
+		blocks[i].addEventListener('click', ()=>{
 			showDetails(data[i], blocks[i].children[1].src);
 			document.querySelector('.details').classList.add('show');
 		})
 	}
-	document.querySelector('.details i').addEventListener('click', () => {
+	document.querySelector('.details i').addEventListener('click', ()=>{
 		document.querySelector('.details').classList.remove('show');
 	})
 }
